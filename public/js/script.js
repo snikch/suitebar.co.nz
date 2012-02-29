09dfd83c4b9ad05b8c48cc9b26a0db397ec56ea4
Mal Curtis <mal@mal.co.nz>
2012-02-29 15:37:22 +1300

    Added pageview tracking for internal hash links

--- public/js/script.js
+++ public/js/script.js
@@ -2,20 +2,21 @@
 
 */
 $(function(){
-	if(window.location.href.indexOf('scroll=true') != -1)
-		$("nav a, .menu-item, header a").click(function(e) {
-			e.preventDefault();
-			var hash = $(this).attr('href');
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			},{
-				duration: 300,
-				easing: 'easeInOutSine',
-				complete: function(){
-					location.hash = hash
-				}
-			});
+	$("nav a, .menu-item, header a").click(function(e) {
+		var hash = $(this).attr('href');
+		_gaq.push(['_trackPageview', hash]);
+		if(window.location.href.indexOf('scroll=true') !== -1) return;
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		},{
+			duration: 300,
+			easing: 'easeInOutSine',
+			complete: function(){
+				location.hash = hash
+			}
 		});
+	});
 	/* Handle cocktails / music z-index */
 	$('.menu-cocktails').hover(function(){
 		$(this).css('z-index', 76);
