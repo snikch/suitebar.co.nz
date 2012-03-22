47312ad54ef67957d09498c922234b3141ea3fbd
Mal Curtis <mal@sitepoint.com>
2012-03-22 20:47:20 +1300

    Using the amazing stamen toner map tiles

--- assets/javascripts/contact.js
+++ assets/javascripts/contact.js
@@ -1,3 +1,7 @@
+(function() {
+
+})();
+
 var maps_callback;
 var Contact = function(){
 	var _this = this, map, suite = [-36.844739,174.763244], el = $('#contact .visual .map').get(0), marker, infowindow, page_center;
@@ -12,7 +16,9 @@ var Contact = function(){
 		cursor.src = 'http://maps.gstatic.com/mapfiles/openhand_8_8.cur';
 	};
 	this.load = function(){
+		_this.load_stamen()
 		page_center = calculate_page_center(); //-36.845880,174.768276
+		var layer = "toner"
 		var opts = {
 			zoom: 16,
 			scrollwheel: false,
@@ -27,9 +33,14 @@ var Contact = function(){
 			streetViewControl: true,
 			streetViewControlOptions: {
 				position: google.maps.ControlPosition.LEFT_CENTER
+			},
+			mapTypeId: layer,
+			mapTypeControlOptions: {
+				mapTypeIds: [layer]
 			}
         }
         map = new google.maps.Map(el, opts);
+		map.mapTypes.set(layer, new google.maps.StamenMapType(layer));
 		_this.pin_suite();
 		_this.setup_streetview();
 	}
@@ -39,7 +50,7 @@ var Contact = function(){
 		var right_offset = 489+110;
 		// 80 = InfoWindow + Icon / 2
 		return [
-			suite[0]+(0.0001*(80/5.82)), 
+			suite[0]+(0.0001*(20/5.82)),
 			suite[1]+(0.0001*Math.round(right_offset/2)/4.666)
 		];
 	}
@@ -76,7 +87,6 @@ var Contact = function(){
 			this.div_ = null;
 			this.content_ = content;
 			this.setMap(map);
-
 		}
 		BlackOverlay.prototype = new google.maps.OverlayView();
 		BlackOverlay.prototype.onAdd = function(){
@@ -105,8 +115,7 @@ var Contact = function(){
 		google.maps.event.addListener(marker, 'click', function() {
 		  infowindow.open(map,marker);
 		});
-		
-		
+
 	};
 	this.setup_streetview = function(){
 		var street_view_opts = {
@@ -128,5 +137,70 @@ var Contact = function(){
 		}
 		var panorama = new google.maps.StreetViewPanorama(el, street_view_opts);
 		map.setStreetView(panorama);
+	}
+	this.load_stamen = function(){
+		var SUBDOMAINS = ["", "a.", "b.", "c.", "d."],
+		PROVIDERS =  {
+			"toner": {
+				"url": "http://{S}tile.stamen.com/toner/{Z}/{X}/{Y}.png",
+				"minZoom": 0,
+				"maxZoom": 20
+			},
+			"toner-lines": {
+				"url": "http://{S}tile.stamen.com/toner-lines/{Z}/{X}/{Y}.png",
+				"minZoom": 0,
+				"maxZoom": 20
+			},
+			"toner-labels": {
+				"url": "http://{S}tile.stamen.com/toner-labels/{Z}/{X}/{Y}.png",
+				"minZoom": 0,
+				"maxZoom": 20
+			},
+			"terrain": {
+				"url": "http://{S}tile.stamen.com/terrain/{Z}/{X}/{Y}.jpg",
+				"minZoom": 4,
+				"maxZoom": 18
+			},
+			"watercolor": {
+				"url": "http://{S}tile.stamen.com/watercolor/{Z}/{X}/{Y}.jpg",
+				"minZoom": 3,
+				"maxZoom": 16
+			}
+		},
+		ATTRIBUTION = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ' +
+			'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. ' +
+			'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ' +
+			'under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.';
+
+		function getProvider(name) {
+			if (name in PROVIDERS) {
+				return PROVIDERS[name];
+			} else {
+				throw 'No such provider: "' + name + '"';
+			}
+		}
+		if (typeof google === "object" && typeof google.maps === "object") {
+			google.maps.StamenMapType = function(name) {
+				var provider = getProvider(name);
+				return google.maps.ImageMapType.call(this, {
+					"getTileUrl": function(coord, zoom) {
+						var index = (zoom + coord.x + coord.y) % SUBDOMAINS.length;
+						return [
+							provider.url
+								.replace("{S}", SUBDOMAINS[index])
+								.replace("{Z}", zoom)
+								.replace("{X}", coord.x)
+								.replace("{Y}", coord.y)
+						];
+					},
+					"tileSize": new google.maps.Size(256, 256),
+					"name":     name,
+					"minZoom":  provider.minZoom,
+					"maxZoom":  provider.maxZoom
+				});
+			};
+			// FIXME: is there a better way to extend classes in Google land?
+			google.maps.StamenMapType.prototype = new google.maps.ImageMapType("_");
+		}
 	}
 }
